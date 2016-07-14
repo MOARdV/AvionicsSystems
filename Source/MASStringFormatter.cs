5934fb885dfe01461fdf90c4dc8fbe8186d0ff12
MOARdV <cwebb@babylonbycandlelight.com>
2016-07-14 14:11:43 -0500

    Add MET/KDT custom formatters

--- Source/MASStringFormatter.cs
+++ Source/MASStringFormatter.cs
@@ -25,7 +25,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using UnityEngine;
 namespace AvionicsSystems
 {
     /// <summary>
@@ -36,7 +36,24 @@ namespace AvionicsSystems
     /// </summary>
     public class MASStringFormatter : IFormatProvider, ICustomFormatter
     {
+        /// <summary>
+        /// Take advantage of KSP's existing date/time formatter.
+        /// </summary>
+        private static KSPUtil.DefaultDateTimeFormatter dateTimeFormatter = new KSPUtil.DefaultDateTimeFormatter();
+
+        /// <summary>
+        /// StringBuilder for concatenating strings.  Keep one persistent
+        /// instance instead of creating one per format task.
+        /// </summary>
         private static StringBuilder sb = new StringBuilder(8);
+
+        private static object[] formatData = new object[8];
+
+        private static readonly int EarthHoursPerDay = 24;
+        private static readonly int KerbinHoursPerDay = 6;
+
+        private static readonly int EarthDaysPerYear = 365;
+        private static readonly int KerbinDaysPerYear = 426;
 
         /// <summary>
         /// Return a reference to ourselves if they're asking for a
@@ -84,8 +101,208 @@ namespace AvionicsSystems
             {
                 return FormatSI(formatSpecification, value);
             }
+            if (formatSpecification.StartsWith("MET") || formatSpecification.StartsWith("KDT"))
+            {
+                return FormatMET(formatSpecification, value);
+            }
 
             return DefaultFormat(formatSpecification, arg, formatProvider);
+        }
+
+        /// <summary>
+        /// Format a numeric value following KSP date/time standards.  For KDT,
+        /// we add one to the days column to mimic KSP behavior.
+        /// </summary>
+        /// <param name="formatSpecification"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private string FormatMET(string formatSpecification, double value)
+        {
+            // vals contains an array of integer values: s, m, h, d, y
+            // All of them are negative if the input is negative.
+            int[] vals;
+            int daysPerYear;
+            int hoursPerDay;
+            if (GameSettings.KERBIN_TIME)
+            {
+                vals = dateTimeFormatter.GetKerbinDateFromUT(value);
+                daysPerYear = KerbinDaysPerYear;
+                hoursPerDay = KerbinHoursPerDay;
+            }
+            else
+            {
+                vals = dateTimeFormatter.GetEarthDateFromUT(value);
+                daysPerYear = EarthDaysPerYear;
+                hoursPerDay = EarthHoursPerDay;
+            }
+
+            char[] chars = formatSpecification.ToCharArray();
+
+            bool calendarAdjust = (chars[1] == 'D');
+            bool negativeValue = (value < 0.0);
+
+            bool activeFormatter = false;
+            bool didSomething = false;
+            char activeChar = ' ';
+            int charCount = 0;
+            int parameterCount = 0;
+            int numChars = chars.Length;
+            int parameterLimit = formatData.Length;
+            for (int i = 0; i < parameterLimit; ++i)
+            {
+                formatData[i] = null;
+            }
+            sb.Remove(0, sb.Length);
+            for (int i = 3; i < numChars && parameterCount < parameterLimit; ++i)
+            {
+                didSomething = false;
+                if (activeFormatter)
+                {
+                    if (chars[i] == activeChar)
+                    {
+                        ++charCount;
+                        didSomething = true;
+                    }
+                    else
+                    {
+                        // Changed format or ended format.  Update SB and format array
+                        activeFormatter = false;
+                        sb.Append('{');
+                        sb.Append(parameterCount);
+                        sb.Append(':');
+                        sb.Append('0', charCount);
+                        sb.Append('}');
+
+                        switch (activeChar)
+                        {
+                            case 'Y':
+                            // Fall through
+                            case 'y':
+                                formatData[parameterCount] = Math.Abs(vals[4]);
+                                break;
+                            case 'D':
+                                formatData[parameterCount] = Math.Abs(vals[3] + daysPerYear * vals[4]);
+                                break;
+                            case 'd':
+                                formatData[parameterCount] = Math.Abs(vals[3] + (calendarAdjust ? 1 : 0));
+                                break;
+                            case 'H':
+                                formatData[parameterCount] = Math.Abs(vals[2] + hoursPerDay * vals[3] + daysPerYear * vals[4]);
+                                break;
+                            case 'h':
+                                formatData[parameterCount] = Math.Abs(vals[2]);
+                                break;
+                            case 'M':
+                                formatData[parameterCount] = Math.Abs(vals[1] + 60 * vals[2] + hoursPerDay * vals[3] + daysPerYear * vals[4]);
+                                break;
+                            case 'm':
+                                formatData[parameterCount] = Math.Abs(vals[1]);
+                                break;
+                            case 'S':
+                                formatData[parameterCount] = Math.Floor(Math.Abs(value));
+                                break;
+                            case 's':
+                                formatData[parameterCount] = Math.Abs(vals[0]);
+                                break;
+                            case 'f':
+                                double fracV = Math.Abs(value) - Math.Floor(Math.Abs(value));
+                                fracV = fracV * Math.Pow(10.0, charCount);
+                                formatData[parameterCount] = Math.Floor(fracV);
+                                break;
+                        }
+                        ++parameterCount;
+                        charCount = 0;
+                    }
+                }
+
+                if (!didSomething)
+                {
+                    switch (chars[i])
+                    {
+                        case 'Y':
+                        case 'y':
+                        case 'D':
+                        case 'd':
+                        case 'H':
+                        case 'h':
+                        case 'M':
+                        case 'm':
+                        case 'S':
+                        case 's':
+                        case 'f':
+                            activeChar = chars[i];
+                            activeFormatter = true;
+                            charCount = 1;
+                            break;
+                        case '+':
+                            sb.Append((negativeValue) ? '-' : '+');
+                            break;
+                        case '-':
+                            if (negativeValue)
+                            {
+                                sb.Append('-');
+                            }
+                            break;
+                        default:
+                            sb.Append(chars[i]);
+                            break;
+                    }
+                }
+            }
+
+            if (activeFormatter && parameterCount < parameterLimit)
+            {
+                activeFormatter = false;
+                sb.Append('{');
+                sb.Append(parameterCount);
+                sb.Append(':');
+                sb.Append('0', charCount);
+                sb.Append('}');
+
+                switch (activeChar)
+                {
+                    case 'Y':
+                    // Fall through
+                    case 'y':
+                        formatData[parameterCount] = Math.Abs(vals[4]);
+                        break;
+                    case 'D':
+                        formatData[parameterCount] = Math.Abs(vals[3] + daysPerYear * vals[4]);
+                        break;
+                    case 'd':
+                        formatData[parameterCount] = Math.Abs(vals[3] + (calendarAdjust ? 1 : 0));
+                        break;
+                    case 'H':
+                        formatData[parameterCount] = Math.Abs(vals[2] + hoursPerDay * vals[3] + daysPerYear * vals[4]);
+                        break;
+                    case 'h':
+                        formatData[parameterCount] = Math.Abs(vals[2]);
+                        break;
+                    case 'M':
+                        formatData[parameterCount] = Math.Abs(vals[1] + 60 * vals[2] + hoursPerDay * vals[3] + daysPerYear * vals[4]);
+                        break;
+                    case 'm':
+                        formatData[parameterCount] = Math.Abs(vals[1]);
+                        break;
+                    case 'S':
+                        formatData[parameterCount] = Math.Floor(Math.Abs(value));
+                        break;
+                    case 's':
+                        formatData[parameterCount] = Math.Abs(vals[0]);
+                        break;
+                    case 'f': 
+                        double fracV = Math.Abs(value) - Math.Floor(Math.Abs(value));
+                        fracV = fracV * Math.Pow(10.0, charCount);
+                        formatData[parameterCount] = Math.Floor(fracV);
+                        break;
+                }
+                charCount = 0;
+                ++parameterCount;
+            }
+
+            string result = string.Format(sb.ToString(), formatData);
+
+            return result;
         }
 
         private static readonly string siPrefixes = " kMGTPEZY";
@@ -96,10 +313,10 @@ namespace AvionicsSystems
         /// <param name="formatSpecification"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string FormatSI(string formatSpecification, double value)
+        private static string FormatSI(string formatSpecification, double value)
         {
             int siChar = 0;
-            if (value >= 1000.0 || value <=-1000.0)
+            if (value >= 1000.0 || value <= -1000.0)
             {
                 bool isNegative = (value < 0.0);
                 siChar = (int)(Math.Log10(Math.Abs(value))) / 3;
