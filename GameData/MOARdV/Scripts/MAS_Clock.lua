4bbb3c76151b30f3e7355524a7377d320b48c3d7
MOARdV <cwebb@babylonbycandlelight.com>
2017-07-01 11:36:13 -0500

    New Clock Lua scripts
    Long (12 char) display, and code to set a manual alarm.

--- GameData/MOARdV/Scripts/MAS_Clock.lua
+++ GameData/MOARdV/Scripts/MAS_Clock.lua
@@ -23,6 +23,19 @@ function MAS_Clock_Time()
 	end
 end
 
+-- Return either UT (mode 1) or MET (mode 2).  Or 0 if the unit is off.
+function MAS_Clock_Time_Long()
+	if fc.GetPersistentAsNumber("MAS_Clock_On") < 1 then
+		return 0
+	elseif fc.GetPersistentAsNumber("MAS_Clock_ClockMode") > 0 then
+		-- return MET
+		return fc.MET()
+	else
+		-- return UT  Add a day because we're using the MET formatter, not KDT.
+		return fc.UT() + 3600 * fc.HoursPerDay()
+	end
+end
+
 -- Return KAC time (if present), or StopWatch time (if active)
 function MAS_Clock_Stopwatch()
 	if fc.GetPersistentAsNumber("MAS_Clock_On") < 1 then
@@ -38,6 +51,28 @@ function MAS_Clock_Stopwatch()
 				return 0
 			elseif stopwatchMode == 1 then
 				return math.min(fc.UT() - fc.GetPersistentAsNumber("MAS_Clock_StopwatchTime"), 3599999)
+			else
+				return fc.GetPersistentAsNumber("MAS_Clock_StopwatchTime")
+			end
+		end
+	end
+end
+
+-- Return KAC time (if present), or StopWatch time (if active)
+function MAS_Clock_Stopwatch_Long()
+	if fc.GetPersistentAsNumber("MAS_Clock_On") < 1 then
+		fc.SetPersistent("MAS_Clock_StopwatchMode", 0)
+		return 0
+	else
+		local alarmTime = kac.TimeToAlarm()
+		if alarmTime > 0 then
+			return alarmTime
+		else
+			local stopwatchMode = fc.GetPersistentAsNumber("MAS_Clock_StopwatchMode")
+			if stopwatchMode == 0 then
+				return 0
+			elseif stopwatchMode == 1 then
+				return (fc.UT() - fc.GetPersistentAsNumber("MAS_Clock_StopwatchTime"))
 			else
 				return fc.GetPersistentAsNumber("MAS_Clock_StopwatchTime")
 			end
@@ -64,4 +99,23 @@ function MAS_Clock_Apsis()
 	else
 		return fc.TimeToPe()
 	end
+end
+
+function MAS_Clock_SetAlarm()
+	if fc.GetPersistentAsNumber("MAS_Clock_On") < 1 then
+		return 0
+	end
+	
+	local alarmTime = fc.GetPersistentAsNumber("MAS_Clock_TimerMins") * 60 + fc.GetPersistentAsNumber("MAS_Clock_TimerSecs")
+	
+	if alarmTime > 0 and kac.Available() > 0 then
+		alarmTime = alarmTime + fc.UT()
+		kac.DeleteAlarm(fc.GetPersistent("MAS_Clock_AlarmId"))
+		local newAlarmId = kac.CreateAlarm("MAS User Alarm", alarmTime)
+		fc.SetPersistent("MAS_Clock_AlarmId", newAlarmId)
+		
+		return 1
+	end
+	
+	return 0
 end
