53a7fb4d2e1d93888947686a978e2cdd3b631755
MOARdV <cwebb@babylonbycandlelight.com>
2017-11-10 11:03:55 -0600

    Support func(object, double, double) dynamic methods
    For new fc.BodyTerrainHeight() method

--- Source/MASFlightComputerVariable.cs
+++ Source/MASFlightComputerVariable.cs
@@ -707,6 +707,11 @@ namespace AvionicsSystems
                             DynamicMethod<object, string, double, double> dm = DynamicMethodFactory.CreateFunc<object, string, double, double>(method);
                             return new Variable(canonical, () => dm(tableInstance, parms[0].String(), parms[1].SafeValue(), parms[2].SafeValue()), cacheable, mutable);
                         }
+                        else if (methodParams[0].ParameterType == typeof(object) && methodParams[1].ParameterType == typeof(double) && methodParams[2].ParameterType == typeof(double))
+                        {
+                            DynamicMethod<object, object, double, double> dm = DynamicMethodFactory.CreateFunc<object, object, double, double>(method);
+                            return new Variable(canonical, () => dm(tableInstance, parms[0].RawValue(), parms[1].SafeValue(), parms[2].SafeValue()), cacheable, mutable);
+                        }
                         else
                         {
                             Utility.LogErrorMessage(this, "!!! GenerateCallVariable(): Don't know how to create variable for {0}, with parameters {1}, {2}, and {3}", canonical, methodParams[0].ParameterType, methodParams[1].ParameterType, methodParams[2].ParameterType);
