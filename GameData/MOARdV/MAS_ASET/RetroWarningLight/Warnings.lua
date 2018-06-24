dbdb7bd51c8dd0f8e685da15fe0c421b95034123
MOARdV <cwebb@babylonbycandlelight.com>
2018-06-24 09:27:55 -0500

    Remove RCS and Ablator from RWL Master Caution

--- GameData/MOARdV/MAS_ASET/RetroWarningLight/Warnings.lua
+++ GameData/MOARdV/MAS_ASET/RetroWarningLight/Warnings.lua
@@ -9,14 +9,12 @@
 
 function MAS_RWL_MasterCaution()
 
-	-- Core resources: propellant, power, RCS, ablator
-	--fc.PropellantStageThreshold(0.0, 0.1) + fc.PowerThreshold(0.0, 0.1) + fc.RcsStageThreshold(0.0, 0.1) + fc.ResourceThreshold("Ablator", 0.0, 0.1)
-	if (fc.PropellantStageThreshold(0.0, 0.1) > 0) or (fc.PowerThreshold(0.0, 0.1) > 0) or (fc.RcsStageThreshold(0.0, 0.1) > 0) or (fc.ResourceThreshold("Ablator", 0.0, 0.1) > 0) then
+	-- Core resources: propellant, power low
+	if (fc.PropellantStageThreshold(0.0, 0.1) > 0) or (fc.PowerThreshold(0.0, 0.1) > 0) then
 		return 1
 	end
 
 	-- Descending, with a parachute, in the atmosphere, and we're in flight
-	-- ((1 - parachute.GetParachuteArmedOrDeployed()) * fc.AtmosphereDepth() * (fc.VerticalSpeed() < 0) * fc.VesselFlying())
 	if (parachute.ParachuteCount() > 0) and (fc.AtmosphereDepth() > 0) and (parachute.GetParachuteArmedOrDeployed() < 0) and (fc.MaxImpactSpeed() + fc.VerticalSpeed() > 0) then
 		return 1
 	end
