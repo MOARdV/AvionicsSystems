6de2e0e1100f478f39c094cccdc79dec01697e8e
MOARdV <cwebb@babylonbycandlelight.com>
2019-01-06 15:10:53 -0600

    Now on v0.92.2 development

--- Properties/SharedAssemblyInfo.cs
+++ Properties/SharedAssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.92.0.*")]
+[assembly: AssemblyVersion("0.92.2.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
