1e375cc75a0df823c215bbd09fea715ad1b0828b
MOARdV <cwebb@babylonbycandlelight.com>
2019-03-01 10:55:53 -0600

    Rename the "Required Mod(s)" field to "Supported Mod(s)"
    In most cases, the mod(s) supplement behavior - they're not actually
    required.

--- Documentor/Documentor.cs
+++ Documentor/Documentor.cs
@@ -1,7 +1,7 @@
 ﻿/*****************************************************************************
  * The MIT License (MIT)
  * 
- * Copyright (c) 2016-2018 MOARdV
+ * Copyright (c) 2016-2019 MOARdV
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -428,7 +428,7 @@ namespace Documentor
                 XmlElement seealso = child["seealso"];
                 if (seealso != null && !string.IsNullOrEmpty(seealso.InnerText.Trim()))
                 {
-                    docString.Append("**Required Mod(s)**: ");
+                    docString.Append("**Supported Mod(s)**: ");
                     docString.AppendLine(seealso.InnerText.Trim());
                     docString.AppendLine();
                 }
