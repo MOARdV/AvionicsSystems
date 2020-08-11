9eb029e1fad93a9093ffc74dc927121cc0fee8ff
MOARdV <cwebb@babylonbycandlelight.com>
2020-08-11 08:47:56 -0500

    Start of RPM Basic MFD

---
+++ GameData/MOARdV/Scripts/MAS_JSI_MFD.lua
@@ -0,0 +1,20 @@
+-- MAS_JSI_MFD.lua
+--
+-- MOARdV's Avionics Systems
+-- Lua scripts for JSI/RPM Basic Multi-Function Displays
+--
+-- Author: MOARdV
+--
+-- This script is public domain (although acknowledgement that MOARdV wrote it would be nice).
+
+-- What does this do?
+--
+-- This script sets defaults for soft keys that can toggle between multiple pages (the 
+-- NavBall/HUD screen being a prime example), so that they'll be able to remember their page.
+
+-- Initialization script for the JSI MFD.  This script may set up multiple variables if needed.
+function MAS_JSI_MFD_Init(propId)
+
+	fc.InitializePersistent(propId .. "-A", "MAS_JSI_BasicMFD_NavBall")
+
+end
