13a311d3e0891cfb27a023354f7622e89e90eadb
MOARdV <cwebb@babylonbycandlelight.com>
2020-08-25 09:04:23 -0500

    Add SelectElectricOutput() Lua function
    For the 5-option electric output props.

--- GameData/MOARdV/MAS_ASET/MAS_ASET_Props.lua
+++ GameData/MOARdV/MAS_ASET/MAS_ASET_Props.lua
@@ -31,3 +31,26 @@ function RCSModeSelect(mode)
 	
 	return 1
 end
+
+-- Function to return a specific electric output rate, depending on the mode.
+-- Used by MAS_DigitalIndicator_Elec_Output and MAS_ASET_Elec_Output.
+--
+-- mode 0: Net output
+-- mode 1: Alternator
+-- mode 2: Fuel Cell
+-- mode 3: Solar
+-- mode 4: Generator
+function SelectElectricOutput(mode)
+
+	if mode == 1 then
+		return fc.AlternatorOutput()
+	elseif mode == 2 then
+		return fc.FuelCellOutput()
+	elseif mode == 3 then
+		return fc.SolarPanelOutput()
+	elseif mode == 4 then
+		return fc.GeneratorOutput()
+	else
+		return fc.AlternatorOutput() + fc.FuelCellOutput() + fc.SolarPanelOutput() + fc.GeneratorOutput()
+	end
+end
